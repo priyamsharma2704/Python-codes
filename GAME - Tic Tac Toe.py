77b4c549a82e9843a24a906197627b74bfc11fe9
priyamsharma2704 <priyamsharma2704@gmail.com>
2017-01-11 20:25:40 -0500

    Final changes in TIC TAC TOE

--- GAME - Tic Tac Toe.py
+++ GAME - Tic Tac Toe.py
@@ -4,12 +4,11 @@ O = "O";
 EMPTY = " ";
 squares = 9 ;
 MAX = [];
-human = "";
-comp = "";
+human1 = "";
+human2 = "";
 x=0;
 def board(MAX):
     
-    print "tttt";
     print "\n\t" , MAX[0], " |" ,MAX[1], "|" ,MAX[2];
     print "\t___|___|___";
     print "\t" , MAX[3], " |" ,MAX[4], "|" ,MAX[5];
@@ -17,12 +16,20 @@ def board(MAX):
     print "\t" , MAX[6], " |" ,MAX[7], "|" ,MAX[8];
     print "\t   |   |   ";
 
+def init_board(MAX):
+    
+    print "\n\t" , "0", " |" ,"1", "|" ,"2";
+    print "\t___|___|___";
+    print "\t" , "3", " |" ,"4", "|" ,"5";
+    print "\t___|___|___";
+    print "\t" , "6", " |" ,"7", "|" ,"8";
+    print "\t   |   |   ";
 
-def  go_h():
+def  go_h1():
     flag = 0;
-    pos  = int( raw_input( "Enter the position : "));
-    while MAX[pos] !=EMPTY and flag == 0:
-        pos  = int( raw_input( " Position Occupied..Enter the position again : "));
+    pos  = int( raw_input( "\nPLAYER 1 : Enter the position : "));
+    while (pos < 0 or pos > 8)or MAX[pos] !=EMPTY and flag == 0 :
+        pos  = int( raw_input( "\nInvalid position..Enter the position again : "));
     for j in range(squares) :
         if pos == j:
             MAX[j] = X;
@@ -30,18 +37,19 @@ def  go_h():
     
 
 
-def go_c():
+def go_h2():
     flag = 0;
-    pos = random.randrange(squares);
-    while MAX[pos] !=EMPTY and flag == 0:
-        pos = random.randrange(squares);
+    pos  = int( raw_input( "\nPLAYER 2 : Enter the position : "));
+    while (pos < 0 or pos > 8) or MAX[pos] !=EMPTY and flag == 0:
+        pos  = int( raw_input( "\nPosition Occupied..Enter the position again : "));
     for j in range(squares) :
         if pos == j:
             MAX[j] = O;                
     flag = 1;
 
 
-def win(MAX):
+def win(MAX , str):
+    str1 = "human1";
     way = ((0,1,2),
            (3,4,5),
            (6,7,8),
@@ -52,36 +60,53 @@ def win(MAX):
            (2,4,6) );
     
     for k in (way):
-        if MAX[k[0]] == MAX[k[1]] == MAX[k[2]] != EMPTY:            
-            print "human winss..!!!";
+        if MAX[k[0]] == MAX[k[1]] == MAX[k[2]] != EMPTY and str1 == str:            
+            print "\nPlayer 1 wins..!!!";
             global x
             x = 1;
             return x;
+        elif MAX[k[0]] == MAX[k[1]] == MAX[k[2]] != EMPTY and str1 != str:            
+            print "\nPlayer 2 wins..!!!";
+            global x
+            x = 1;
+            return x;
+        elif EMPTY not in MAX :
+            print "\nITS A TIE..!!!";
+            global x
+            x = 1;
+            return x; 
+            
         
-    
 
 ans = raw_input("You want to go first? Press (y/n)");
-print ans.lower();
+while ans[0].lower() != 'y' and ans[0].lower() != 'n' :
+    ans = raw_input("\nInvalid value..Press (y/n)");
 for i in range(squares):
     MAX.append(EMPTY);
-#board(MAX);
+init_board(MAX);
 if ans[0] == 'y' or ans[0] == 'Y':
-    human = X;
-    comp = O;
-    while x !=1:
-        print "x = " , x;
-        go_h();
-        go_c();
+    human1 = X;
+    human2 = O;
+    while x !=1 and EMPTY in MAX:
+        go_h1();        
         board(MAX);
-        win(MAX);
-        #result();
-        
-        
-    
+        win(MAX , "human1");
+        if x !=1:
+            go_h2();
+            board(MAX);
+            win(MAX , "human2");
+           
 else :
-    #print"noo";
-    human = O;
-    comp = X;
+    human1 = O;
+    human2 = X;
+    while x !=1 and EMPTY in MAX:
+        go_h2();        
+        board(MAX);
+        win(MAX,"human2");
+        if x !=1:
+            go_h1();
+            board(MAX);
+            win(MAX,"human1");
     
 
 
